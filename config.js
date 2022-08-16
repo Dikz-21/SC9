42eca5e03e7a716285208b36c2229cc1faff0f1a
zuls <109126733+Dikz-21@users.noreply.github.com>
2022-08-16 22:49:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
+global.owner = ['6285839814393']
+global.premium = ['6285839814393']
+global.packname = 'ZulsBot√'
+global.author = 'ZulsBot-MD'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
